4f90fd9de162e1300ba6ed17ce7f8f2f8eb88186
Matias Tong <matias.tong@gmail.com>
2020-06-27 22:02:40 -0400

    read all and associate method

--- superherosightings/src/main/java/com/sms/superherosightings/dao/HeroDaoImpl.java
+++ superherosightings/src/main/java/com/sms/superherosightings/dao/HeroDaoImpl.java
@@ -5,11 +5,15 @@
  */
 package com.sms.superherosightings.dao;
 
+import com.sms.superherosightings.dao.HeroDaoImpl.HeroMapper;
 import com.sms.superherosightings.model.Hero;
 import com.sms.superherosightings.model.Organization;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 
@@ -25,14 +29,24 @@ public class HeroDaoImpl implements Dao<Hero> {
        jdbc.update(INSERT_HERO, model.getHeroId(),model.getName(),model.getDescription(),model.getSuperpower(), model.getType());
        int newId = jdbc.queryForObject("SELECT Last_Insert_Id()", Integer.class);
        model.setHeroId(newId);
+       insertHeroOrganization(model);
        return model;
     }
     
-    private void insertHeroOrganization()
-
+    private void insertHeroOrganization(Hero model){
+       List<Organization> organizations = model.getOrganization();
+       for(Organization organization: organizations){
+           final String INSERT_HERO_ORGANIZATION = "INSERT INTO HeroOrganization(HeroId, OrganizationId) VALUES (?,?);";
+           jdbc.update(INSERT_HERO_ORGANIZATION, model.getHeroId(), organization.getOrganizationId());
+       }
+    
+    }
+   
     @Override
     public List<Hero> readAll() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        final String SELECT_ALL_HEROES = "SELECT * FROM Hero";
+        List<Hero> heroes = jdbc.query(SELECT_ALL_HEROES, new HeroMapper());
+        return heroes;        
     }
 
     @Override
@@ -51,12 +65,31 @@ public class HeroDaoImpl implements Dao<Hero> {
     }
     
     private List<Organization> getOrganizationsByHero(int heroId){
-    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    final String sql = "SELECT o.* FROM Organization o JOIN HeroOrganization ho ON o.OrganizationId "
+            + "= ho.OrganizationId WHERE ho.HeroId = ?";
+    List<Organization> organizations = jdbc.query(sql, new OrganizationMapper(), heroId);
+    return organizations;
     }
     
-    private void associateOrganizations(int heroId, List<Organization> organization){
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    private void associateOrganizations(List ){
+    List<Organization> organizations = getOrganizationsByHero(heroId);
+    
     }
+    
+    public static final class HeroMapper implements RowMapper<Hero>{
+
+        @Override
+        public Hero mapRow(ResultSet rs, int arg1) throws SQLException {
+            Hero hero = new Hero();
+            hero.setHeroId(rs.getInt("HeroId"));
+            hero.setName(rs.getString("`Name`"));
+            hero.setDescription(rs.getString("`Description`"));
+            hero.setSuperpower(rs.getString("Specialty"));
+            hero.setType(rs.getString("`Type`"));
+            return hero;
+        }
+    
+}
 
 
     
