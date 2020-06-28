e852db8d393844956a8ef8ab932766a5064ee39f
Matias Tong <matias.tong@gmail.com>
2020-06-27 23:46:43 -0400

    added test for HeroDaoImpl

---
+++ superherosightings/src/test/java/com/sms/superherosightings/dao/HeroDaoImplTest.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sms.superherosightings.dao;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author matiastong
+ */
+public class HeroDaoImplTest {
+    
+    public HeroDaoImplTest() {
+    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }
+
+    /**
+     * Test of create method, of class HeroDaoImpl.
+     */
+    @Test
+    public void testCreate() {
+    }
+
+    /**
+     * Test of readAll method, of class HeroDaoImpl.
+     */
+    @Test
+    public void testReadAll() {
+    }
+
+    /**
+     * Test of readById method, of class HeroDaoImpl.
+     */
+    @Test
+    public void testReadById() {
+    }
+
+    /**
+     * Test of update method, of class HeroDaoImpl.
+     */
+    @Test
+    public void testUpdate() {
+    }
+
+    /**
+     * Test of delete method, of class HeroDaoImpl.
+     */
+    @Test
+    public void testDelete() {
+    }
+    
+}
