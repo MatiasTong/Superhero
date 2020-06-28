2c74b8c52dbfa3f9f1917616034ec87c880994a2
Sangay Yolmo <sangyec@Sangays-MacBook-Pro.local>
2020-06-28 12:04:19 -0400

    Testing

--- superherosightings/src/main/java/com/sms/superherosightings/dao/LocationDaoImpl.java
+++ superherosightings/src/main/java/com/sms/superherosightings/dao/LocationDaoImpl.java
@@ -25,7 +25,7 @@ public class LocationDaoImpl implements Dao<Location> {
 
     @Override
     public List<Location> readAll() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return null;
     }
 
     @Override
