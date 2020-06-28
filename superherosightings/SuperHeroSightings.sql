ade556b82f194c5fd60e5647cb1a92e351e546fe
Matias Tong <matias.tong@gmail.com>
2020-06-27 20:25:27 -0400

    added sql scripts

---
+++ superherosightings/SuperHeroSightings.sql
@@ -0,0 +1,55 @@
+DROP DATABASE IF EXISTS SuperHeroSightings;
+CREATE DATABASE SuperHeroSightings;
+USE SuperHeroSightings;
+
+CREATE TABLE Hero(
+	HeroId INT PRIMARY KEY AUTO_INCREMENT,
+    `Name` VARCHAR(30) NOT NULL,
+    `Description` VARCHAR(30) NOT NULL,
+    Specialty VARCHAR(30) NOT NULL,
+    `Type` VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE Location(
+	LocationId INT PRIMARY KEY AUTO_INCREMENT,
+    `Name` VARCHAR(30) NOT NULL,
+    `Description` VARCHAR(30) NOT NULL,
+	Address VARCHAR(30) NOT NULL,
+    City VARCHAR(30) NOT NULL,
+    State CHAR(2) NOT NULL,
+    ZipCode CHAR(5) NOT NULL,
+    Lat VARCHAR(10) NOT NULL,
+    `Long` VARCHAR(10) NOT NULL
+);
+
+CREATE TABLE Organization(
+	OrganizationId INT PRIMARY KEY AUTO_INCREMENT,
+    `Name` VARCHAR(30) NOT NULL,
+    `Description` VARCHAR(30) NOT NULL,
+     LocationId INT NOT NULL,
+     FOREIGN KEY FK_Organization_Location_LocationId(LocationId) references Location(LocationId),
+     Email VARCHAR(30) NOT NULL,
+    `Type` VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE HeroOrganization(
+	HeroId INT,
+    OrganizationId INT,
+    PRIMARY KEY(HeroId, OrganizationId),
+    FOREIGN KEY FK_HeroOrganization_Hero_HeroId(HeroId) references Hero(HeroId),
+	FOREIGN KEY FK_HeroOrganization_Organization_OrganizationId(OrganizationId) references `Organization`(OrganizationId)
+);
+
+CREATE TABLE Sighting(
+	SightingId INT PRIMARY KEY AUTO_INCREMENT,
+    Location INT,
+    HeroId INT,
+    FOREIGN KEY FK_Sighting_Location_LocationId(LocationId) references Location(LocationId),
+    FOREIGN KEY FK_Sighting5_Hero_HeroId(HeroId) references Hero(HeroId)
+);
+
+
+
+
+
+
